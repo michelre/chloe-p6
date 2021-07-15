90512b5baa9e7e02dca35063f9a3a5d60ee66122
Chloé R <clositi@live.fr>
2021-07-15 19:22:08 +0200

    fetch and display data in html TEST

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// fetch data from json file
 fetch("FishEyeData.json")
 	.then(function (response) {
-	/*		if (!response.ok) {
+		/*		if (!response.ok) {
 			throw new Error("HTTP error " + response.status);
 		}
 	*/
@@ -8,7 +9,17 @@ fetch("FishEyeData.json")
 	})
 	.then(function (data) {
 		console.log(data);
+		appendData(data.photographers);
 	})
 	.catch(function (err) {
 		console.log(err);
 	});
+
+function appendData(foo) {
+	var mainContainer = document.getElementById("js_Data");
+	for (var i = 0; i < foo.length; i++) {
+		var div = document.createElement("div");
+		div.innerHTML = foo[i].name;
+		mainContainer.appendChild(div);
+	}
+}
