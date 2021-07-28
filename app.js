9c5ef93dab5223d46b96e210307a030d5c6a269b
Chloé R <clositi@live.fr>
2021-07-28 15:21:55 +0200

    created photographs class to display data on index improved style of cards with sass

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { Photographer } from "./js/photographerData.js";
+
 // fetch data from json file
 fetch("FishEyeData.json")
 	.then(function (response) {
@@ -6,34 +8,12 @@ fetch("FishEyeData.json")
 	})
 	.then(function (data) {
 		console.log(data);
+		for (let i = 0; i < data.photographers.length; i++) {
+			const photographer = new Photographer(data.photographers[i]);
+			photographer.createPhotographerTags();
+			photographer.createPhotographerCard();
+		}
 	})
 	.catch(function (err) {
 		console.log(err);
 	});
-
-//DOM Elements
-const wrapper = document.querySelector("#js_Data");
-
-//Header
-const imgLogo = document.createElement("img");
-const linkLogo = document.createElement("a");
-const header = document.createElement("header");
-wrapper.appendChild(header).appendChild(linkLogo).appendChild(imgLogo);
-imgLogo.src = "img/logo.svg";
-imgLogo.alt = "Fisheye Home page";
-linkLogo.href = "index.html";
-
-//Nav
-const nav = document.createElement("nav");
-header.appendChild(nav);
-
-//Main
-const mainContainer = document.createElement("main");
-wrapper.appendChild(mainContainer);
-
-//Element - content button
-const contentButton = document.createElement("button");
-contentButton.classList.add("button--content");
-contentButton.classList.add("button");
-mainContainer.appendChild(contentButton);
-contentButton.textContent = "Passer au contenu";
--- js/photographerData.js
+++ js/photographerData.js
@@ -9,44 +9,44 @@ export class Photographer {
 		this.tagline = data.tagline;
 		this.tags = data.tags;
 	}
-}
+	createPhotographerTags() {
+		let tagsPhotographer = "";
+		for (let i = 0; i < this.tags.length; i++) {
+			return (tagsPhotographer += `<span class="photographer__tags__tag">#${this.tags[i]}</span>`);
+		}
+	}
 
-export const createPhotographerTags = () => {
-	const tagsPhotographer = document.querySelector(".photographer__tags");
-	for (let i = 0; i < this.tags.length; i++)
-		tagsPhotographer.innerHTML += `<span class="photographer__tags__tag">#${this.tag[i]}</span>`;
-};
-
-export const createPhotographerCard =() =>{
-	const mainContainer = document.querySelector(".mainContainer");
-	mainContainer.innerHTML +=`
+	createPhotographerCard() {
+		const mainContainer = document.querySelector(".mainContainer");
+		mainContainer.innerHTML += `
 	<article class="photographer">
 		<a alt="${this.name}" href="photograph.html" class="photographer__link">
 			<div class="photographer__picture">
-				<img src="medias/thumbnails/${this.portrait}" alt="">
+				<img src="img/thumbnails/${this.portrait}" alt="">
 			</div>
-			<h2 class="photographer__name">${this.name}</div>
+			<h2 class="photographer__name">${this.name}</h2>
 		</a>
 		<div class="photographer__location">${this.city}, ${this.country}</div>
 		<div class="photographer__tagline">${this.tagline}</div>
-		<div class="photographer__price">${this.price}</div>
-		<div class="photographer__tags"></div>
+		<div class="photographer__price">${this.price}€/jour</div>
+		<div class="photographer__tags">${this.createPhotographerTags()}</div>
 	</article>`;
-};
+	}
 
-export const createPhotographerHeader =() =>{
-	const mainContainer = document.querySelector(".mainContainer");
-	mainContainer.innerHTML +=`
+	createPhotographerHeader() {
+		const mainContainer = document.querySelector(".mainContainer");
+		mainContainer.innerHTML += `
 	<section class="photographer__header">
 		<div class="photographer__description">			
 			<h1 class="photographer__name">${this.name}</div>
 			<div class="photographer__location">${this.city}, ${this.country}</div>
 			<div class="photographer__tagline">${this.tagline}</div>
-			<div class="photographer__tags"></div>
+			<div class="photographer__tags">${this.createPhotographerTags()}</div>
 		</div>
 		<div photographer__contact></div>
 		<div class="photographer__picture">
-			<img src="medias/thumbnails/${this.portrait}" alt="${this.name}">
+			<img src="img/thumbnails/${this.portrait}" alt="${this.name}">
 		</div>
 	</section>`;
-};
+	}
+}
